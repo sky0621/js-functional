1b1faac4c96405fa15967b21a1de9335686e9ec6
sky0621 <sky14work@gmail.com>
2018-02-17 23:55:10 +0900

    continue

--- ch04/list0414.js
+++ ch04/list0414.js
@@ -1,5 +1,14 @@
-var expect = request('expect.js');
 
-// 空のリスト
-var empty = () => {};
+// 空のリストを生成する（もっともシンプルな起点となる関数）
+var empty = () => {
+    return [];
+};
+console.log(empty());
 
+// valueをlistに格納する
+var construct = (value, list) => {
+    list[0] = value;
+    return list;
+};
+var oneValueList = construct("one", empty());
+console.log(oneValueList);
